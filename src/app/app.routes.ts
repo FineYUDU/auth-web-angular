c1423e2609c1394b6629aff568229e81dd971078
FineYudu <fine_567@hotmail.com>
2025-12-04 19:38:46 -0600

    routes:auth/main,auth/login, auth/create-account created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'auth',
+        children:[
+            {
+                path:'main',
+                loadComponent:()=> import('./auth/views/main/main')
+            },
+            {
+                path:'login',
+                loadComponent:()=> import('./auth/views/login/login')
+            },
+            {
+                path:'create-accout',
+                loadComponent:()=> import('./auth/views/create-accout/create-accout')
+            },
+            { path:'**', pathMatch:'full', redirectTo:'main'}
+        ]
+    },
+    { path:'**', pathMatch:'full', redirectTo:'auth'}
+];
---
+++ src/app/auth/views/create-accout/create-accout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-accout',
+  imports: [],
+  templateUrl: './create-accout.html',
+  styleUrl: './create-accout.css',
+})
+export default class CreateAccout {
+
+}
