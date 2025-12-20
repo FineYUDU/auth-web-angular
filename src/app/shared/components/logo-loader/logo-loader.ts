d97e0b47181b22476ed655e350d9c0023dc75d30
FineYudu <fine_567@hotmail.com>
2025-12-20 15:48:51 -0600

    dark and light loader logo added

--- src/app/shared/components/logo-loader/logo-loader.ts
+++ src/app/shared/components/logo-loader/logo-loader.ts
@@ -1,5 +1,6 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Theme } from 'yudu-component-kit';
 
 @Component({
   selector: 'logo-loader',
@@ -13,5 +14,7 @@ import { Component } from '@angular/core';
   }
 })
 export class LogoLoader {
+
+  public theme = inject( Theme );
 
 }
